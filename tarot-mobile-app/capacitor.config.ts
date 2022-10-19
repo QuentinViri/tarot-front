5a4253bd57f3374fad058c2e3ae414d63b40c680
Quentin <quentin.viricel98@gmail.com>
2022-10-19 11:05:38 +0200

    init commit

---
+++ tarot-mobile-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'tarot-mobile-app',
+  webDir: 'dist/tarot-mobile-app',
+  bundledWebRuntime: false
+};
+
+export default config;
